12c783b03962e9aca867b78ce747dac226f5fb47
Giuseppe Guerra <to@nyo.zz.mu>
2020-03-17 23:59:15 +0100

    More comments, changed privilege names

--- privileges/privileges.go
+++ privileges/privileges.go
@@ -3,9 +3,17 @@ package privileges
 // Privileges represents the privileges that a given telegram user has on the bot
 type Privileges int
 
+// User privilege constants
 const (
+	// Can use volunteer features
 	Normal Privileges = 1 << iota
+
+	// Can use the admin panel
 	Admin
-	AdminAddArea
-	AdminAddOrder
+
+	// Can manage areas
+	AdminManageAreas
+
+	// Can manage orders
+	AdminManageOrders
 )
--- statemodels/order.go
+++ statemodels/order.go
@@ -1,5 +1,6 @@
 package statemodels
 
+// Order is the state model of the admin/order state
 type Order struct {
 	OrderID uint
 }
